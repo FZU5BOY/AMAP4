e5351b421a4dd13376c00a96bd5d5d56082b9a78
Zeashon <285391347@qq.com>
2015-08-05 16:10:40 +0800

    2015.8.5  16：21  部分资源损坏重传、   zeashon

---
+++ app/src/main/java/com/example/amap/activity/writeCommActivity.java
@@ -0,0 +1,18 @@
+package com.example.amap.activity;
+
+import android.app.Activity;  
+import android.os.Bundle;
+
+import com.example.amap.R;
+
+public class writeCommActivity extends Activity{  
+  
+    @Override  
+    protected void onCreate(Bundle savedInstanceState) {  
+        // TODO Auto-generated method stub  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.write_comm);
+          
+    }  
+      
+} 
