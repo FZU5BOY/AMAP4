2e203f59b9b4439c91dd62f261155acc3954052c
Zeashon <285391347@qq.com>
2015-08-06 22:42:00 +0800

    2015.8.6  22：22  XListViewActivity 跳转评论页BUG修复、   zeashon

--- app/src/main/java/com/example/amap/activity/WriteComActivity.java
+++ app/src/main/java/com/example/amap/activity/WriteComActivity.java
@@ -1,17 +1,30 @@
 package com.example.amap.activity;
 
-import android.app.Activity;  
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.amap.R;
 
 public class WriteComActivity extends Activity{
-  
+    private Button submit_commBtn;
     @Override  
     protected void onCreate(Bundle savedInstanceState) {  
         // TODO Auto-generated method stub  
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.write_comm);
+        submit_commBtn = (Button) findViewById(R.id.submit_comm);
+        submit_commBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("zjx", "comment finished,click to shop detail");
+                Intent intent = new Intent(WriteComActivity.this, XListViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }  
       
 } 
