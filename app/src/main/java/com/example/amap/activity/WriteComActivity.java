63f0f185dea26e12d9d830def3e4076c51adcf26
Zeashon <285391347@qq.com>
2015-08-11 10:33:05 +0800

    2015.8.11  10：31  商店评论、   zeashon

--- app/src/main/java/com/example/amap/activity/WriteComActivity.java
+++ app/src/main/java/com/example/amap/activity/WriteComActivity.java
@@ -6,7 +6,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+import java.util.List;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import com.example.amap.R;
 
 public class WriteComActivity extends Activity{
@@ -16,15 +38,19 @@ public class WriteComActivity extends Activity{
         // TODO Auto-generated method stub  
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.write_comm);
-        submit_commBtn = (Button) findViewById(R.id.submit_comm);
-        submit_commBtn.setOnClickListener(new View.OnClickListener() {
+//        comment event
+        Button submitBtn = (Button) findViewById(R.id.submit_comm);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Log.i("zjx", "comment finished,click to shop detail");
-                Intent intent = new Intent(WriteComActivity.this, XListViewActivity.class);
-                startActivity(intent);
+                // TODO Auto-generated method stub
+                HttpClient httpClient = new DefaultHttpClient();
+                String url = "http://192.168.191.1/write_comm.php";
+                uploadData(url, httpClient);
             }
         });
+//        cancel event
         Button btnCancel = (Button) findViewById(R.id.cancel_comm);
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +60,47 @@ public class WriteComActivity extends Activity{
                 startActivity(intent);
             }
         });
-    }  
-      
-} 
+    }
+//    data upload
+    private void uploadData(String url, HttpClient httpClient) {
+        EditText comm_content = (EditText) findViewById(R.id.comm_content);
+        RatingBar rate_stars = (RatingBar) findViewById(R.id.rate_stars);
+        String commContent = comm_content.getText().toString();
+        String rateStars = String.valueOf(rate_stars.getRating());
+
+        List<BasicNameValuePair> params = new LinkedList<BasicNameValuePair>();
+        params.add(new BasicNameValuePair("username", "中国"));
+        params.add(new BasicNameValuePair("comment", commContent));
+        params.add(new BasicNameValuePair("stars", rateStars));
+
+        try {
+            HttpPost postMethod = new HttpPost(url);
+            postMethod.setEntity(new UrlEncodedFormEntity(params, "utf-8")); // 将参数填入POST
+            // Entity中
+            HttpResponse httpResponse = httpClient.execute(postMethod); // 执行POST方法
+
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                //String httpResult = EntityUtils.toString(
+                //	httpResponse.getEntity(), "utf-8");
+                Toast.makeText(getApplicationContext(), "评论成功！",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }else{
+                Toast.makeText(getApplicationContext(), "网络错误",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+
+    }
+}
--- app/src/main/java/com/example/amap/activity/XListViewActivity.java
+++ app/src/main/java/com/example/amap/activity/XListViewActivity.java
@@ -1,12 +1,26 @@
 package com.example.amap.activity;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 
 import com.example.amap.R;
 import com.example.amap.view.xlist.XListView;
 import com.example.amap.view.xlist.XListView.IXListViewListener;
 import com.example.amap.activity.WriteComActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
@@ -18,23 +32,39 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 
 public class XListViewActivity extends Activity implements IXListViewListener {
     private final String TAG = "XListViewActivity";
     private XListView mListView;
-    //	private ArrayAdapter<String> mAdapter;
-//	private ArrayList<String> items = new ArrayList<String>();
+    // private ArrayAdapter<String> mAdapter;
+    // private ArrayList<String> items = new ArrayList<String>();
     private Handler mHandler;
     ArrayList<HashMap<String, Object>> listItem;
-    private int j = 0;
+    private int addMoreLmtS = 10;
     MyAdapter mAdapter;
     private boolean marked = false;
-//	private static int refreshCnt = 0;
+    String url = "http://192.168.191.1/get_comm.php";
+
+    // private static int refreshCnt = 0;
 
     /**
      * Called when the activity is first created.
@@ -43,34 +73,28 @@ public class XListViewActivity extends Activity implements IXListViewListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xlistview);
-        geneItems();
+        HttpClient httpClient = new DefaultHttpClient();
+        geneItems(url, httpClient);
         mListView = (XListView) findViewById(R.id.xListView);
         mListView.setPullLoadEnable(true);
-//		List<Map<String, Object>> list = geneItems();
+        // List<Map<String, Object>> list = geneItems();
         mAdapter = new MyAdapter(this);
         mListView.setAdapter(mAdapter);
-//		mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
-//		mListView.setAdapter(mAdapter);
-//		mListView.setPullLoadEnable(false);
-//		mListView.setPullRefreshEnable(false);
+        // mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
+        // mListView.setAdapter(mAdapter);
+        // mListView.setPullLoadEnable(false);
+        // mListView.setPullRefreshEnable(false);
         mListView.setXListViewListener(this);
         mHandler = new Handler();
-        Typeface icons = Typeface.createFromAsset(getAssets(), "fonts/iconfont.ttf");
+        Typeface icons = Typeface.createFromAsset(getAssets(),
+                "fonts/iconfont.ttf");
         Button btn1 = (Button) findViewById(R.id.go_here);
         Button btn2 = (Button) findViewById(R.id.mark_place);
         Button btn3 = (Button) findViewById(R.id.write_comm);
         btn1.setTypeface(icons);
         btn2.setTypeface(icons);
         btn3.setTypeface(icons);
-//        back to index
-        Button btnGoBack = (Button) findViewById(R.id.detail_GoBack);
-        btnGoBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(XListViewActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+
         btn2.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -94,42 +118,83 @@ public class XListViewActivity extends Activity implements IXListViewListener {
                 Intent intent = new Intent();
                 intent.setClass(XListViewActivity.this, WriteComActivity.class);
                 startActivity(intent);
-//                finish();//停止当前的Activity,如果不写,则按返回键会跳转回原来的Activity        
+                // finish();//停止当前的Activity,如果不写,则按返回键会跳转回原来的Activity
             }
 
         });
     }
 
-    private ArrayList<HashMap<String, Object>> geneItems() {
-        listItem = new ArrayList<HashMap<String, Object>>();
-        /**为动态数组添加数据*/
-//		int j=i+30;
-        for (int i = 1; i < 30; i++) {
-            HashMap<String, Object> map = new HashMap<String, Object>();
-            String user = "匿名用户";
-            map.put("avatar", "dsb");
-            map.put("stars", "4");
-            map.put("username", user);
-            map.put("comm_time", "8月1日");
-            map.put("comm_content", "不错");
-            listItem.add(map);
-        }
+    private ArrayList<HashMap<String, Object>> geneItems(String url,
+                                                         HttpClient httpClient) {
+        getComms(url, httpClient, true, "0");
+        addMoreLmtS = 10;
         return listItem;
     }
 
-    private ArrayList<HashMap<String, Object>> addItems() {
-//		listItem = new ArrayList<HashMap<String,Object>>();
-        /**为动态数组添加数据*/
-//		int j=i+30;
-        for (int i = 1; i < 30; i++) {
-            HashMap<String, Object> map = new HashMap<String, Object>();
-            String user = "匿名用户";
-            map.put("avatar", "dsb");
-            map.put("stars", "3");
-            map.put("username", user);
-            map.put("comm_time", "8.1");
-            map.put("comm_content", "挺好的");
-            listItem.add(map);
+    private ArrayList<HashMap<String, Object>> addItems(String url,
+                                                        HttpClient httpClient) {
+        // listItem = new ArrayList<HashMap<String,Object>>();
+        String lmtS = String.valueOf(addMoreLmtS);
+        /** 为动态数组添加数据 */
+        getComms(url, httpClient, false, lmtS);
+        addMoreLmtS += 10;
+        return listItem;
+    }
+
+    private ArrayList<HashMap<String, Object>> getComms(String url,
+                                                        HttpClient httpClient, boolean newListItem, String lmtS) {
+        if (newListItem) {
+            listItem = new ArrayList<HashMap<String, Object>>();
+        }
+
+        List<BasicNameValuePair> params = new LinkedList<BasicNameValuePair>();
+        params.add(new BasicNameValuePair("lmtS", lmtS));
+
+        try {
+            HttpPost postMethod = new HttpPost(url);
+            postMethod.setEntity(new UrlEncodedFormEntity(params, "utf-8")); // 将参数填入POST
+            // Entity中
+            HttpResponse httpResponse = httpClient.execute(postMethod); // 执行POST方法
+
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                String httpResult = EntityUtils.toString(
+                        httpResponse.getEntity(), "utf-8");
+                try {
+                    JSONArray result = new JSONArray(httpResult);
+                    if (result.length() == 0) {
+                        Toast.makeText(getApplicationContext(), "已加载全部评论",
+                                Toast.LENGTH_SHORT).show();
+                        return null;
+                    }
+                    /** 为动态数组添加数据 */
+                    for (int i = 0; i < result.length(); i++) {
+                        JSONObject jsonObject = result.getJSONObject(i);
+                        HashMap<String, Object> map = new HashMap<String, Object>();
+                        map.put("avatar", "dsb");
+                        map.put("stars", jsonObject.getString("stars"));
+                        map.put("username", jsonObject.getString("username"));
+                        map.put("comm_time", jsonObject.getString("time"));
+                        map.put("comm_content", jsonObject.getString("comment"));
+                        listItem.add(map);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Toast.makeText(getApplicationContext(), "网络错误",
+                        Toast.LENGTH_SHORT).show();
+            }
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            httpClient.getConnectionManager().shutdown();
         }
         return listItem;
     }
@@ -137,7 +202,7 @@ public class XListViewActivity extends Activity implements IXListViewListener {
     private void onLoad() {
         mListView.stopRefresh();
         mListView.stopLoadMore();
-//		mListView.setRefreshTime("刚刚");  不知道为什么合并这里会报错、暂时注释、
+//        mListView.setRefreshTime("刚刚");
     }
 
     @Override
@@ -146,14 +211,15 @@ public class XListViewActivity extends Activity implements IXListViewListener {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-//				start = ++refreshCnt;
-//				items.clear();
-                j++;
-                geneItems();
+                // start = ++refreshCnt;
+                // items.clear();
+                HttpClient httpClient = new DefaultHttpClient();
+                geneItems(url, httpClient);
                 mAdapter.notifyDataSetChanged();
-//				mAdapter = new ArrayAdapter<String>(XListViewActivity.this, R.layout.list_item, items);
-//				MyAdapter mAdapter = new MyAdapter(XListViewActivity.this);
-//				mListView.setAdapter(mAdapter);
+                // mAdapter = new ArrayAdapter<String>(XListViewActivity.this,
+                // R.layout.list_item, items);
+                // MyAdapter mAdapter = new MyAdapter(XListViewActivity.this);
+                // mListView.setAdapter(mAdapter);
                 onLoad();
             }
         }, 2000);
@@ -165,8 +231,8 @@ public class XListViewActivity extends Activity implements IXListViewListener {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                j++;
-                addItems();
+                HttpClient httpClient = new DefaultHttpClient();
+                addItems(url, httpClient);
                 mAdapter.notifyDataSetChanged();
                 onLoad();
             }
@@ -177,7 +243,7 @@ public class XListViewActivity extends Activity implements IXListViewListener {
         private LayoutInflater mInflater = null;
 
         private MyAdapter(Context context) {
-            //根据context上下文加载布局，这里的是Demo17Activity本身，即this
+            // 根据context上下文加载布局，这里的是Demo17Activity本身，即this
             this.mInflater = LayoutInflater.from(context);
         }
 
@@ -200,25 +266,31 @@ public class XListViewActivity extends Activity implements IXListViewListener {
         }
 
         @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView,
+                            ViewGroup parent) {
             // TODO Auto-generated method stub
             if (convertView == null) {
                 convertView = mInflater.inflate(R.layout.vlist, null);
             }
-            ImageView avatar = (ImageView) convertView.findViewById(R.id.avatar);
+            ImageView avatar = (ImageView) convertView
+                    .findViewById(R.id.avatar);
             RatingBar stars = (RatingBar) convertView.findViewById(R.id.stars);
             TextView user = (TextView) convertView.findViewById(R.id.username);
             TextView time = (TextView) convertView.findViewById(R.id.comm_time);
-            TextView content = (TextView) convertView.findViewById(R.id.comm_content);
-            int resID = getResources().getIdentifier(listItem.get(position).get("avatar").toString(), "drawable", "com.example.amap");
+            TextView content = (TextView) convertView
+                    .findViewById(R.id.comm_content);
+            int resID = getResources().getIdentifier(
+                    listItem.get(position).get("avatar").toString(),
+                    "drawable", "org.com.cctest");
             avatar.setImageResource(resID);
-            stars.setRating((float) Integer.parseInt(listItem.get(position).get("stars").toString()));
+            stars.setRating((float) Integer.parseInt(listItem.get(position)
+                    .get("stars").toString()));
             user.setText(listItem.get(position).get("username").toString());
             time.setText(listItem.get(position).get("comm_time").toString());
-            content.setText(listItem.get(position).get("comm_content").toString());
+            content.setText(listItem.get(position).get("comm_content")
+                    .toString());
             return convertView;
         }
     }
-
 
 }
